231cabfc12fe9fc14adcc0061c9151ebd76e1bd1
Laplandin <lopatin@ucoz-team.net>
2020-01-27 00:18:11 +0300

    fix gitignore

--- bin/init.ts
+++ bin/init.ts
@@ -2,18 +2,18 @@ import request from 'graphql-request';
 import * as fixtures from '../prisma/fixtures/roles.query';
 
 try {
-	(async () => {
-		console.log('Applying fixtures to db...');
+    (async () => {
+        console.log('Applying fixtures to db...');
 
-		for await (const query of fixtures) {
+        for await (const query of fixtures) {
             const res = await request('http://localhost:4466', query);
             console.log(res);
         }
 
-		console.log('Everything is ok, bye');
-		process.exit(0);
-	})();
+        console.log('Everything is ok, bye');
+        process.exit(0);
+    })();
 } catch (err) {
-	console.error('err', err);
-	process.exit(1);
+    console.error('err', err);
+    process.exit(1);
 }
